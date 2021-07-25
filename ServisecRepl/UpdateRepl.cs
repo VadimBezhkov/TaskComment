6d5a56d68bfb09dea39e047dc0bc866f22fae34e
Вадим Бежков <vadimbezhkov3112@gmail.com>
2021-07-25 22:44:26 +0300

    Work

---
+++ ServisecRepl/UpdateRepl.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using Repositories.Repositor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServisecRepl
+{
+   public class UpdateRepl
+    {
+        private ReplRepository _replRepository = new ReplRepository(new DataProvider.CommentContext());
+        public void Update(Repl rep)
+        {
+            _replRepository.UpdateRepl(rep);
+        }
+    }
+}
